9260486bd1271b300a0b64fd47da72d138bc6270
lrgeofflewis <LRGeoff@LRWorkComputer.cyber>
2015-01-17 01:38:39 +0000

    restful app to stub json titles

---
+++ register-stub/getjson.rb
@@ -0,0 +1,25 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+#function that reads a formatted json file in and returns as pure json response
+def getjson(filename)
+  #checks if file exists and goes to failed page if it doesn't
+  if File.file?("../files/"+filename)
+    json = File.read("../files/"+filename)
+    obj = JSON.parse(json)
+    return obj.to_json
+    else
+      redirect "../failed"
+  end
+end
+
+#trying to get list of files to put in index page automatically but not yet complete!
+def filename
+  files = Hash.new
+  filenames =  Dir.glob('/vagrant/files/*.json')
+  filenames.foreach(".") do |file|
+   puts file
+  files[file].add file
+  end
+  return files.to_json
+end
